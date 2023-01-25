153eb4820c3896143dd5cf3b3349a698cae71e46
Владислав <95309036+Kwikli@users.noreply.github.com>
2023-01-25 18:43:16 +0200

    day7

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+
+const store = createStore({
+    state: {
+       cart: []
+     },
+     mutations: {
+         setCart: (state, tovar) => state.cart.push(tovar),
+     },
+     getters: {
+         getCart: (state) =>
+         {
+             return state.cart
+         }
+     }  
+ })
+export default store;
